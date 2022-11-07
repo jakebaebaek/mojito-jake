afded500d63c4fda021c5885c44abf8f608d3466
jakebaebaek <87072502+jakebaebaek@users.noreply.github.com>
2022-11-08 00:21:11 +0900

    Add files via upload

---
+++ src/component/main_page/Main.js
@@ -0,0 +1,19 @@
+import Top100 from './Top100.js';
+import Filter from './Filter.js';
+import mainStyles from './Main.module.css'
+
+
+
+function Main(){
+
+    return (
+        <div className={mainStyles.mainPage}>
+            <Top100 />
+            <Filter />
+        </div>
+    );
+}
+
+
+
+export default Main;
