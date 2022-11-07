89accfc0df1e220fa2768e2d1fca88675a3ca05a
jakebaebaek <87072502+jakebaebaek@users.noreply.github.com>
2022-11-08 00:21:36 +0900

    Add files via upload

---
+++ src/redux/root-reducer.js
@@ -0,0 +1,28 @@
+import { combineReducers } from 'redux';
+import { configureStore, createSlice , createAsyncThunk} from "@reduxjs/toolkit";
+import { persistReducer , createMigrate,} from 'redux-persist';
+// localstorage에 저장
+import storage from 'redux-persist/lib/storage';
+import storageSession from 'redux-persist/lib/storage/session';
+import getEmailReducer  from './getEmail.js';
+import getMemberStoreReducer from './getStore.js'
+import getEmojiReducer  from './getEmoji.js';
+// import bucketReducer from './emojiThunk.js'
+
+const persistConfig = {
+    version: 1,
+    key: 'root',
+    storage: storage,
+    // whitelist: ["email"],
+    blacklist:['emoji']
+};
+
+
+const rootReducer = combineReducers({
+  email : getEmailReducer.reducer,
+  store : getMemberStoreReducer.reducer,
+  emoji : getEmojiReducer.reducer,
+  // bucket : bucketReducer
+})
+
+export default persistReducer(persistConfig, rootReducer)
